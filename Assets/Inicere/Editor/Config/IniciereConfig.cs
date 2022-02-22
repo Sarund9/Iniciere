98face3842fd32b9fc8037dea962c684b549e09c
Sarund9 <sebi.obrutsky@gmail.com>
2022-02-21 21:04:14 -0300

    Better Config Code

--- Assets/Inicere/Editor/Config/IniciereConfig.cs
+++ Assets/Inicere/Editor/Config/IniciereConfig.cs
@@ -1,15 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using UnityEditor;
 using UnityEngine;
 
 namespace Iniciere
 {
     public class IniciereConfig : ScriptableObject
     {
-        internal static IniciereConfig Instance;
 
         //[SerializeField]
         //List<EditorFolder> editorFolders = new List<EditorFolder>();
@@ -17,13 +18,47 @@ namespace Iniciere
         //[SerializeField]
         //List<string> projectNamespaces = new List<string>();
 
-        public string projectNamespace;
-        public string projectEditorFolder;
-        public string projectEditorNamespace; 
+        public string projectNamespace;         // Implemented
+        public string projectEditorFolder;      // --
+        public string projectEditorNamespace;   // --
 
+        public static IniciereConfig Instance { get; private set; }
 
-        //public IList EditorFolders => editorFolders;
-        //public IList ProjectNamespaces => projectNamespaces;
+        [InitializeOnLoadMethod]
+        private static void OnInitialize()
+        {
+            FetchConfig();
+        }
+        private static void FetchConfig()
+        {
+            while (true)
+            {
+                if (Instance != null) return;
+
+                var path = GetConfigPath();
+
+                if (path == null)
+                {
+                    AssetDatabase.CreateAsset(CreateInstance<IniciereConfig>(), $"Assets/{nameof(IniciereConfig)}.asset");
+                    Debug.Log("INICIERE: A config file has been created at the root of the project.");
+                    continue;
+                }
+
+                Instance = AssetDatabase.LoadAssetAtPath<IniciereConfig>(path);
+
+                break;
+            }
+        }
+        private static string GetConfigPath()
+        {
+            var paths = AssetDatabase.FindAssets(nameof(IniciereConfig))
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Where(c => c.EndsWith(".asset"))
+                .ToList();
+            if (paths.Count > 1) Debug.LogWarning("Multiple auto save config assets found. Delete one.");
+            return paths.FirstOrDefault();
+        }
+
     }
 
     [Serializable]
--- Assets/Inicere/Editor/Config/IniciereConfigEditor.cs
+++ Assets/Inicere/Editor/Config/IniciereConfigEditor.cs
@@ -7,41 +7,7 @@ namespace Iniciere
     [CustomEditor(typeof(IniciereConfig))]
     public class IniciereConfigEditor : Editor
     {
-        [InitializeOnLoadMethod]
-        private static void OnInitialize()
-        {
-            FetchConfig();
-        }
-        private static void FetchConfig()
-        {
-            while (true)
-            {
-                if (IniciereConfig.Instance != null) return;
-
-                var path = GetConfigPath();
-
-                if (path == null)
-                {
-                    AssetDatabase.CreateAsset(CreateInstance<IniciereConfig>(), $"Assets/{nameof(IniciereConfig)}.asset");
-                    Debug.Log("INICIERE: A config file has been created at the root of the project.");
-                    continue;
-                }
-
-                IniciereConfig.Instance = AssetDatabase.LoadAssetAtPath<IniciereConfig>(path);
-
-                break;
-            }
-        }
-        private static string GetConfigPath()
-        {
-            var paths = AssetDatabase.FindAssets(nameof(IniciereConfig))
-                .Select(AssetDatabase.GUIDToAssetPath)
-                .Where(c => c.EndsWith(".asset"))
-                .ToList();
-            if (paths.Count > 1) Debug.LogWarning("Multiple auto save config assets found. Delete one.");
-            return paths.FirstOrDefault();
-        }
-
+        
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
